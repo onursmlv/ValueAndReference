1d32cb13d71eb2326dd66346f5564a0a55e4a3b1
0nurkt <onur.ismayilov@kibrit.tech>
2024-03-06 20:36:22 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Text;
+
+namespace Value___Reference_Types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //a = 7;
+            //Console.WriteLine(a);
+
+
+            //int a = 5;
+            //int b = a;
+
+            //Console.WriteLine(b);
+
+            //b = 10;
+            //Console.WriteLine(a);
+
+            //Console.WriteLine(b);
+
+            //int[] num1 = { 100, 200, 300 };
+            //int[] num2;
+            //num2 = num1;
+
+            //num2[0] = 200;
+
+            //string name1 = "Reshad";
+            //string name2;
+
+            //name2 = name1;
+
+            //name2 = "Onur";
+
+            //name2 += " Salam";
+
+            //Console.WriteLine(name1);
+            //Console.WriteLine(name2);
+
+            //StringBuilder name1 = new StringBuilder();
+
+            //StringBuilder name2 = new StringBuilder();
+
+            //name1.Append("Garage");
+            //name2 = name1;
+            //name2.Append("Academy");
+            //name1.Append("555");
+
+            //Console.WriteLine(name1);
+            //Console.WriteLine(name2);
+
+
+            // ref and out
+
+            //int a = 5;
+
+            //changeNumber(ref a);
+
+            //Console.WriteLine(a);
+
+            //int a =10;
+            //changeNumber(ref a);
+            //Console.WriteLine(a);
+        }
+
+        //static int changeNumber(ref int a)
+        //{
+        //    return a;
+        //}
+
+        //static int changeNumber(out int a)
+        //{
+        //    a = 10;
+        //    return a;
+        //}
+    }
+}
